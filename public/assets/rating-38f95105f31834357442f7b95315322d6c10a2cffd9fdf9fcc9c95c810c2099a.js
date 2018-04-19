3e784f3bde28c3a05f803dddc2fc1b3cf65db335
Jon Janelle <jjanelle@g.harvard.edu>
2018-04-18 22:07:47 -0400

    precompile assets

---
+++ public/assets/rating-38f95105f31834357442f7b95315322d6c10a2cffd9fdf9fcc9c95c810c2099a.js
@@ -0,0 +1,39 @@
+var Rating = {
+  init: function() {
+    $(".star").tooltip();
+    $('#stars li').on('mouseover', function () {
+      var onStar = parseInt($(this).data('value'), 10); // The star currently mouse on
+
+      // Now highlight all the stars that's not after the current hovered star
+      $(this).parent().children('li.star').each(function (e) {
+        if (e < onStar) {
+          $(this).addClass('hover');
+        } else {
+          $(this).removeClass('hover');
+        }
+      });
+
+    }).on('mouseout', function () {
+      $(this).parent().children('li.star').each(function (e) {
+        $(this).removeClass('hover');
+      });
+    });
+
+    $('#stars li').on('click', function () {
+      var onStar = parseInt($(this).data('value'), 10); // The star currently selected
+      var stars = $(this).parent().children('li.star');
+
+      for (i = 0; i < stars.length; i++) {
+        $(stars[i]).removeClass('selected');
+      }
+
+      for (i = 0; i < onStar; i++) {
+        $(stars[i]).addClass('selected');
+      }
+
+      var ratingValue = parseInt($('#stars li.selected').last().data('value'), 10);
+      $("#rating").val(ratingValue);
+    });
+  }
+}
+;
